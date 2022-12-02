dc047edb95495e109a0959c960192a771e0b9a32
ChaCha3088 <cha3088@gmail.com>
2022-12-02 16:41:34 +0900

    feat(/src/main/java/subway/domain/Subway.java): add 지하철 역 이름은 2글자 이상이어야 한다.

---
+++ src/main/java/subway/domain/SubwayVerification.java
@@ -0,0 +1,10 @@
+package subway.domain;
+
+public class SubwayVerification {
+    public static String verifyStationName(String stationName) {
+        if (stationName.length() < 2) {
+            throw new IllegalArgumentException("[ERROR] 지하철 역 이름은 2글자 이상이어야 한다.")
+        }
+        return stationName;
+    }
+}
